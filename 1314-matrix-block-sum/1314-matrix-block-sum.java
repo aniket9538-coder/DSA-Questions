0b5c8d276885f708905c1e246b8b2b30832b9701
Aniket sandip sawant <sawantaniket071@gmail.com>
2026-07-06 19:24:25 +0530

    Time: 7 ms (68.93%), Space: 46.5 MB (78.29%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.java
+++ 1314-matrix-block-sum/1314-matrix-block-sum.java
@@ -1,34 +1,52 @@
 class Solution {
+
     public int[][] matrixBlockSum(int[][] mat, int k) {
 
         int m = mat.length;
         int n = mat[0].length;
 
-        int[][] answer = new int[m][n];
+        // Extra row and column for easier calculation
+        int[][] prefix = new int[m + 1][n + 1];
 
-        for(int i = 0; i < m; i++) {
+        // Build Prefix Matrix
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
 
-            for(int j = 0; j < n; j++) {
-
-                int sum = 0;
-
-                for(int r = i-k; r <= i+k; r++) {
-
-                    for(int c = j-k; c <= j+k; c++) {
-
-                        if(r >= 0 && r < m && c >= 0 && c < n) {
-
-                            sum += mat[r][c];
-
-                        }
-                    }
-                }
-
-                answer[i][j] = sum;
-
+                prefix[i][j] =
+                        mat[i - 1][j - 1]
+                      + prefix[i - 1][j]
+                      + prefix[i][j - 1]
+                      - prefix[i - 1][j - 1];
             }
         }
 
-        return answer;
+        int[][] ans = new int[m][n];
+
+        // Find Block Sum for every cell
+        for (int i = 0; i < m; i++) {
+
+            for (int j = 0; j < n; j++) {
+
+                int r1 = Math.max(0, i - k);
+                int c1 = Math.max(0, j - k);
+
+                int r2 = Math.min(m - 1, i + k);
+                int c2 = Math.min(n - 1, j + k);
+
+                // Shift by +1 because prefix has extra row & column
+                r1++;
+                c1++;
+                r2++;
+                c2++;
+
+                ans[i][j] =
+                        prefix[r2][c2]
+                      - prefix[r1 - 1][c2]
+                      - prefix[r2][c1 - 1]
+                      + prefix[r1 - 1][c1 - 1];
+            }
+        }
+
+        return ans;
     }
 }
